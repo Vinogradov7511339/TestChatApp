a6130ae1d287a99de7ee6028581ea745ab6ff38d
Alexander Vinogradov <vinogradov7511339@gmail.com>
2021-10-25 14:39:22 +0300

    added user model

---
+++ TestChatApp/GlobalConstants.swift
@@ -0,0 +1,10 @@
+//
+//  GlobalConstants.swift
+//  TestChatApp
+//
+//  Created by Alexander Vinogradov on 25.10.2021.
+//
+
+import Foundation
+
+public let kCurrentUser = "currentUser"
---
+++ TestChatApp/Models/User.swift
@@ -0,0 +1,44 @@
+//
+//  User.swift
+//  TestChatApp
+//
+//  Created by Alexander Vinogradov on 25.10.2021.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct User: Codable, Equatable {
+
+    let id: String
+    let username: String
+    let email: String
+    let pushId: String?
+    let avatarLink: String?
+    let status: String
+
+    static var currentId: String? {
+        return Auth.auth().currentUser?.uid
+    }
+
+    static var currentUser: User? {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.data(forKey: kCurrentUser) else {
+            return nil
+        }
+
+        let decoder = JSONDecoder()
+        do {
+            let user = try decoder.decode(User.self, from: data)
+            return user
+        } catch {
+            assert(false, "decode failure: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
